64411a81852942259eb596994dc3ed55a69f23b3
DRY411S <DRY411S@users.noreply.github.com>
2016-08-30 17:26:16 +0100

    v0.14.01
    No functional changes

--- migrations/RemoveChemicalBatteries_0.13.00.lua
+++
@@ -1,7 +0,0 @@
--- Enables the mod in saved worlds where mod was not previously used
-for index, nextforce in pairs(game.forces) do
-	nextforce.reset_recipes()
-	nextforce.reset_technologies()
-
-end
-
